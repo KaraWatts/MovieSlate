fb650cd1a589ffcb12c9ca124f42272879f57899
Kara Watts <kdwwatts@gmail.com>
2024-01-29 23:39:56 -0500

    started scripting drag and drop functionality

---
+++ script.js
@@ -0,0 +1,63 @@
+document.querySelector('#searchButton').addEventListener('click', addMovie);
+const movieList = document.querySelector('#movieRanking');
+
+
+//Add an item to the movie list
+function addMovie() {
+    //create list item for new movies
+    let listItem = document.createElement('li');
+    listItem.setAttribute('draggable', 'true');
+    listItem.setAttribute('class', 'listedMovie')
+    listItem.innerHTML = document.querySelector('#searchBar').value;
+    //create item for drag markers
+    let dragHamburger = document.createElement('i')
+    dragHamburger.setAttribute('class', 'draggableMarker')
+    listItem.appendChild(dragHamburger)
+    
+    // Add dragging class to listed movie on dragstart event
+    listItem.addEventListener('dragstart', () => {
+        listItem.classList.add("dragging");
+    });
+    // Remove dragging class from item on dragend event
+    listItem.addEventListener("dragend", () => listItem.classList.remove("dragging"));
+    
+    //add new list item to Movie List
+    movieList.appendChild(listItem);
+    saveData();
+    
+}
+
+showRankings();
+
+
+
+function draggableMovieList(e) {
+    e.preventDefault();
+    const draggingItem = document.querySelector(".dragging");
+    // Getting all items except currently dragging and making array of them
+    let siblings = [...movieList.querySelectorAll(".listedMovie:not(.dragging)")];
+    
+    // Finding the sibling after which the dragging item should be placed
+    let nextSibling = siblings.find(sibling => {
+        return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
+    });
+    // Inserting the dragging item before the found sibling
+    movieList.insertBefore(draggingItem, nextSibling);
+    saveData();
+}
+
+movieList.addEventListener("dragover", draggableMovieList);
+movieList.addEventListener("dragenter", e => e.preventDefault());
+
+
+//Save the movie list to the local storage
+function saveData() {
+    localStorage.setItem("data", movieList.innerHTML);
+}
+
+//show saved movie rankings on window load
+function showRankings() {
+    movieList.innerHTML = localStorage.getItem('data');
+}
+
+showRankings();
